f75bc5e455c466928bec7ed59097bed38b03c7ee
Anderson <Anderson@ANDERSON1>
2018-05-09 22:45:09 -0400

    Add address sources, exceptions and stall to IF Stage

--- src/Stage_IF.v
+++ src/Stage_IF.v
@@ -6,17 +6,23 @@
 // Actually only contains the PC register and a 4 bytes adder
 
 
-module StageIF(clk_i, rst_i, out);
+module StageIF(clk_i, rst_i, branch_addr, sel_addr, stall, mis_addr_exc, out);
   input clk_i;
   input rst_i;
 
+  input [31:0] branch_addr;
+  input sel_addr;
+  input stall;
+  output mis_addr_exc;
+  output [31:0] out;
+
   reg [31:0] PC = 0;
-  output wire [31:0] out;
 
   assign out = PC;
+  assign mis_addr_exc = PC[0] | PC[1];
 
   always @(posedge clk_i) begin
-    if(!rst_i)
-      PC += 4;
+    if(!rst_i & !stall)
+      PC = (sel_addr) ? branch_addr : PC + 4;
   end
 endmodule
--- tests/Stage_IF_tb.cpp
+++ tests/Stage_IF_tb.cpp
@@ -13,6 +13,20 @@
 #define ERROR_COLOR "\033[0;31m"
 #define NO_COLOR    "\033[m"
 
+
+#define SEL_ADDR        0
+#define STALL           1
+#define BRANCH_ADDR     2
+#define MIS_ADDR_EXC    3
+#define OUTPUT          4
+
+#define SEL_ADDR_PC4    0
+#define SEL_ADDR_BR     1
+#define STALL_ON        1
+#define STALL_OFF       0
+
+#define TOTAL_TESTS 15
+
 using namespace std;
 
 class SIMULATIONTB: public Testbench<VStage_IF> {
@@ -24,25 +38,36 @@ class SIMULATIONTB: public Testbench<VStage_IF> {
     int Simulate(unsigned long max_time=1000000){
       Reset();
 
-      bool ok = true;
-      int exit_code;
 
-      int verify_data = 0;
+      // Test data   [Addr_Src | Stall | Branch Address | Exception Flag | Output]
+      int unsigned data[TOTAL_TESTS][5] = {                    //                  OUTPUT   | STALL | EXCEPTION
+        {SEL_ADDR_PC4, STALL_OFF, 0x00004000, 0, 0x00000004},  // PC = PC + 4  = 0x00000004 |  NO   |    NO
+        {SEL_ADDR_PC4, STALL_OFF, 0xDEADF00D, 0, 0x00000008},  // PC = PC + 4  = 0x00000008 |  NO   |    NO
+        {SEL_ADDR_PC4, STALL_ON,  0x00000000, 0, 0x00000008},  // PC = PC      = 0x00000008 |  YES  |    NO
+        {SEL_ADDR_PC4, STALL_ON,  0x00000000, 0, 0x00000008},  // PC = PC      = 0x00000008 |  YES  |    NO
+        {SEL_ADDR_PC4, STALL_ON,  0x00000000, 0, 0x00000008},  // PC = PC      = 0x00000008 |  YES  |    NO
+        {SEL_ADDR_BR,  STALL_OFF, 0x00040000, 0, 0x00040000},  // PC = BR_ADDR = 0x00040000 |  NO   |    NO
+        {SEL_ADDR_BR,  STALL_OFF, 0x00050001, 1, 0x00050001},  // PC = BR_ADDR = 0x00050001 |  NO   |    YES
+        {SEL_ADDR_BR,  STALL_ON,  0x00000004, 1, 0x00050001},  // PC = PC      = 0x00050001 |  YES  |    YES
+        {SEL_ADDR_BR,  STALL_OFF, 0x00000080, 0, 0x00000080},  // PC = BR_ADDR = 0x00000080 |  NO   |    NO
+        {SEL_ADDR_PC4, STALL_OFF, 0x00000080, 0, 0x00000084},  // PC = PC + 4  = 0x00000084 |  NO   |    NO
+        {SEL_ADDR_BR,  STALL_OFF, 0x0000DEAD, 1, 0x0000DEAD},  // PC = BR_ADDR = 0x0000DEAD |  NO   |    YES
+        {SEL_ADDR_PC4, STALL_ON,  0x00000000, 1, 0x0000DEAD},  // PC = PC      = 0x0000DEAD |  YES  |    YES
+        {SEL_ADDR_BR,  STALL_ON,  0xABCDEF00, 1, 0x0000DEAD},  // PC = PC      = 0x0000DEAD |  YES  |    YES
+        {SEL_ADDR_BR,  STALL_OFF, 0xABCDEF00, 0, 0xABCDEF00},  // PC = BR_ADDR = 0xABCDEF00 |  NO   |    NO
+        {SEL_ADDR_PC4, STALL_OFF, 0x00000000, 0, 0xABCDEF04}}; // PC = PC + 4  = 0xABCDEF04 |  NO   |    NO
+      
+      for (int num_test = 0; num_test < TOTAL_TESTS; num_test++) {
+        m_core->branch_addr = data[num_test][BRANCH_ADDR];
+        m_core->sel_addr    = data[num_test][SEL_ADDR];
+        m_core->stall       = data[num_test][STALL] ;
 
-      for (; getTime() < max_time;) {
         Tick();
-  
-        verify_data += 4;
 
-        if(m_core->out != verify_data)
-          ok = false;     
+        if((m_core->mis_addr_exc != data[num_test][MIS_ADDR_EXC]) ||
+           (m_core->out != data[num_test][OUTPUT]))
+          return num_test;
       }
-      if(ok)
-        exit_code = 0;
-      else
-        exit_code = 1;
-        
-      return exit_code;
     }
 };
 
@@ -57,10 +82,10 @@ int main(int argc, char **argv, char **env) {
 
   printf("\nStage IF Testbench:\n");
 
-  if(!ret)
-    printf(OK_COLOR "[OK]" NO_COLOR " Test Passed!");
+  if(ret == TOTAL_TESTS)
+    printf(OK_COLOR "[OK]" NO_COLOR " Test Passed! Complete: %d/%d", ret, TOTAL_TESTS);
   else
-    printf(ERROR_COLOR "[FAILED]" NO_COLOR " Test Failed!");
+    printf(ERROR_COLOR "[FAILED]" NO_COLOR " Test Failed! Complete: %d/%d", ret, TOTAL_TESTS);
 
   printf("\n");
 
