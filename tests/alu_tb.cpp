e54dff389beaddf85b52783a59f365ee04484793
Anderson <Anderson@ANDERSON1>
2018-05-18 16:39:37 -0400

    Update ALU. Add equ, lt and ltu output flags

--- tests/alu_tb.cpp
+++ tests/alu_tb.cpp
@@ -18,6 +18,9 @@
 #define IN1      1
 #define OP       2
 #define OUT      3
+#define EQU      4
+#define LT       5
+#define LTU      6
 
 #define ALU_ADD   0b0000
 #define ALU_SUB   0b1000
@@ -31,7 +34,7 @@
 #define ALU_SLTU  0b0011
 
 
-#define TOTAL_TESTS 16
+#define TOTAL_TESTS 24
 
 
 using namespace std;
@@ -45,24 +48,32 @@ class SIMULATIONTB: public Testbench<Valu> {
     int Simulate(unsigned long max_time=1000000){
       Reset();
 
-      // Test data
-      int data[TOTAL_TESTS][4] = {
-        {          5,     6,   ALU_ADD,         11},    // 5 + 6 = 11
-        {        -20,     5,   ALU_ADD,        -15},    // -20 + 5 = -15
-        {       -187,   187,   ALU_ADD,          0},    // -187 + 187 = 0
-        {          4,     3,   ALU_SUB,          1},    // 4 - 3 = 1
-        {        -23,  -127,   ALU_SUB,        104},    // -23 - (-127) = 104
-        {        245,   120,   ALU_AND,        112},    // 245 & 120 = 112
-        {        100,    50,   ALU_OR,         118},    // 100 | 50 = 118
-        {        573,   215,   ALU_XOR,        746},    // 573 ^ 215 = 746
-        {       1024,     3,   ALU_SRL,        128},    // 1024 >> 4 = 128
-        {-2147483648,     5,   ALU_SRL,   67108864},    // -2147483648 >> 5 = 67108864
-        {-2147483648,     5,   ALU_SRA,  -67108864},    // -2147483648 >> 5 = -67108864
-        {         64,     5,   ALU_SLL,       2048},    // 64 << 5 = 2048
-        {          1,    10,   ALU_SLT,          1},    // 1 < 10 = true
-        {         20,    10,   ALU_SLT,          0},    // 20 < 10 = false
-        {         10,     0,   ALU_SLTU,         1},    // 10 != 0 = true
-        {          0,     0,   ALU_SLTU,         0}};   // 0 != 0 = false
+      // Test data    [in_0 | in_1 | ALU OP | Output | EQU | LT | LTU]
+      int data[TOTAL_TESTS][7] = {                               //             Operation          | Equal | Less than | Less than (unsigned)
+        {          5,     6,   ALU_ADD,         11, 0, 1, 1},    // 5 + 6 = 11                     |   0   |     1     |       1
+        {        -20,     5,   ALU_ADD,        -15, 0, 1, 0},    // -20 + 5 = -15                  |   0   |     1     |       0
+        {       -187,   187,   ALU_ADD,          0, 0, 1, 0},    // -187 + 187 = 0                 |   0   |     1     |       0
+        {          4,     3,   ALU_SUB,          1, 0, 0, 0},    // 4 - 3 = 1                      |   0   |     0     |       0
+        {        -23,  -127,   ALU_SUB,        104, 0, 0, 0},    // -23 - (-127) = 104             |   0   |     0     |       0
+        {         -1,     0,   ALU_SUB,         -1, 0, 1, 0},    // -1 - 0 = -1                    |   0   |     1     |       0
+        {        120,   245,   ALU_AND,        112, 0, 1, 1},    // 245 & 120 = 112                |   0   |     1     |       1
+        {        540,   540,   ALU_AND,        540, 1, 0, 0},    // 540 & 540 = 540                |   1   |     0     |       0
+        {        100,    50,   ALU_OR,         118, 0, 0, 0},    // 100 | 50 = 118                 |   0   |     0     |       0
+        {          0,     0,   ALU_OR,           0, 1, 0, 0},    // 0 | 0 = 0                      |   1   |     0     |       0
+        {        573,   215,   ALU_XOR,        746, 0, 0, 0},    // 573 ^ 215 = 746                |   0   |     0     |       0
+        {       4500,  4500,   ALU_XOR,          0, 1, 0, 0},    // 4500 ^ 4500 = 0                |   1   |     0     |       0
+        {       1024,     3,   ALU_SRL,        128, 0, 0, 0},    // 1024 >> 4 = 128                |   0   |     0     |       0
+        {-2147483648,     5,   ALU_SRL,   67108864, 0, 1, 0},    // -2147483648 >> 5 = 67108864    |   0   |     1     |       0
+        {-2147483648,     5,   ALU_SRA,  -67108864, 0, 1, 0},    // -2147483648 >> 5 = -67108864   |   0   |     1     |       0
+        {       2048,     1,   ALU_SRA,       1024, 0, 0, 0},    // 2048 >> 1 = 1024               |   0   |     1     |       0
+        {         64,     5,   ALU_SLL,       2048, 0, 0, 0},    // 64 << 5 = 2048                 |   0   |     0     |       0
+        {         -2,     1,   ALU_SLL,         -4, 0, 1, 0},    // -2 << 1 = -4                   |   0   |     1     |       0
+        {          1,    10,   ALU_SLT,          1, 0, 1, 1},    // 1 < 10 = true                  |   0   |     1     |       1
+        {         20,    10,   ALU_SLT,          0, 0, 0, 0},    // 20 < 10 = false                |   0   |     0     |       0
+        {        -20,   -10,   ALU_SLT,          1, 0, 1, 1},    // -20 < -10 = true               |   0   |     1     |       1
+        {         50,   800,   ALU_SLTU,         1, 0, 1, 1},    // 50 < 800 = true (Unsigned)     |   0   |     1     |       1
+        {         30,    20,   ALU_SLTU,         0, 0, 0, 0},    // 30 < 20 = false (Unsigned)     |   0   |     0     |       0
+        {        100,  -200,   ALU_SLTU,         1, 0, 0, 1}};   // 100 < -200 = true (Unsigned)   |   0   |     0     |       1
 
 
       for (int num_test = 0; num_test < TOTAL_TESTS; num_test++) {
@@ -72,7 +83,8 @@ class SIMULATIONTB: public Testbench<Valu> {
 
         Tick();
 
-        if(m_core->out_o != data[num_test][OUT])
+        if((m_core->out_o != data[num_test][OUT]) || (m_core->equ_o != data[num_test][EQU]) ||
+           (m_core->lt_o != data[num_test][LT]) || (m_core->ltu_o != data[num_test][LTU]))
           return num_test;
       }
     }
