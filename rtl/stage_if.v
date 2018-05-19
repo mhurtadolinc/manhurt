b429284773d42ed2c913ff3d0f338f56658c7f24
Anderson <Anderson@ANDERSON1>
2018-05-19 14:27:02 -0400

    Fix rst_i logic in stage_if

--- rtl/stage_if.v
+++ rtl/stage_if.v
@@ -11,15 +11,15 @@ module stage_if(clk_i, rst_i, branch_addr_i, sel_addr_i, stall_i, mis_addr_exc_o
   input sel_addr_i;
   input stall_i;
   output mis_addr_exc_o;
-  output [31:0] pc_o;
+  output reg [31:0] pc_o;
 
-  reg [31:0] PC = 0;
+  assign mis_addr_exc_o = pc_o[0] | pc_o[1];
 
-  assign pc_o = PC;
-  assign mis_addr_exc_o = PC[0] | PC[1];
-
+  // PC's
   always @(posedge clk_i) begin
-    if(!rst_i & !stall_i)
-      PC = (sel_addr_i) ? branch_addr_i : PC + 4;
+    if (rst_i)
+      pc_o = 0;
+    else if (!stall_i)
+      pc_o = (sel_addr_i) ? branch_addr_i : pc_o + 4;
   end
 endmodule
