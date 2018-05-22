761e8a5d3402d2c154889a25c80feedf4459eea5
Adrian <adgp1997@hotmail.com>
2018-05-22 15:21:16 -0400

    Added more tests to the immediate generator

--- tests/imm_gen_tb.cpp
+++ tests/imm_gen_tb.cpp
@@ -19,15 +19,15 @@
 #define IMM_OUT       2
 
 
-#define IMM_I    0b000
-#define IMM_S    0b001
-#define IMM_B    0b010
-#define IMM_U    0b011
-#define IMM_J    0b100
-#define IMM_C    0b101
+#define IMM_I    0
+#define IMM_S    1
+#define IMM_B    2
+#define IMM_U    3
+#define IMM_J    4
+#define IMM_C    5
 
 
-#define TOTAL_TESTS 7
+#define TOTAL_TESTS 13
 
 
 using namespace std;
@@ -42,16 +42,22 @@ class SIMULATIONTB: public Testbench<Vimm_gen> {
       Reset();
 
       // Test data        
-      long long int data[TOTAL_TESTS][3] = {       // Instruction | Imm_op | Imm_out
-        {0x00700000, IMM_I,          7},    // 0x00700000  |    I   |    7
-        {0x3A000000, IMM_I,        928},    // 0x3A000000  |    I   |    928
-        {0x6B700000, IMM_I,       1719},     // 0xE5600000  |    I   |   256
-        {0x60B0A000, IMM_U,       1622188032},     // 0xE5600000  |    I   |   256
-        {0x00001000, IMM_U,       4096},         // 0xE5600000  |    I   |   256
-        {0x53B33000, IMM_S,       226618},           // 0xE5600000  |    I   |   256
-        {0xE5600000, IMM_S,       -426}};        // 0xE5600000  |    I   |   256
+      long long int data[TOTAL_TESTS][3] = {              // Instruction | Imm_op |    Imm_out
+        {0x00700000, IMM_I,                7},            // 0x00700000  |    I   |       7
+        {0x3A000000, IMM_I,              928},            // 0x3A000000  |    I   |      928
+        {0x6B700000, IMM_I,             1719},            // 0x6B700000  |    I   |      1719
+        {0x60B0A000, IMM_U,       1622188032},            // 0x60B0A000  |    U   |   1622188032
+        {0x00001000, IMM_U,             4096},            // 0x00001000  |    U   |      4096
+        {0x53B33000, IMM_S,             1312},            // 0x53B33000  |    S   |      1312
+        {0xE5600000, IMM_S,       4294966848},            // 0xE5600000  |    S   |      -448   when truncated to 32 bits
+        {0x6B15555F, IMM_B,             1706},            // 0x6B15555F  |    B   |      1706
+        {0xEB15555F, IMM_B,       4294964906},            // 0xEB15555F  |    B   |     -2390   when truncated to 32 bits
+        {0x6732AD0E, IMM_J,           175730},            // 0x6732AD0E  |    J   |     175730
+        {0xE1D550F7, IMM_J,       4294270492},            // 0xE1D550F7  |    J   |    -696804  when truncated to 32 bits
+        {0x6732AD0E, IMM_C,                5},            // 0x6732AD0E  |    J   |        5
+        {0xE1D550F7, IMM_C,               10}             // 0xE1D550F7  |    J   |       10
+        }; 
 
-      
       int num_test;
 
       for (num_test = 0; num_test < TOTAL_TESTS; num_test++) {
@@ -61,15 +67,15 @@ class SIMULATIONTB: public Testbench<Vimm_gen> {
         Tick();
 
         if(m_core->imm_o != data[num_test][IMM_OUT])
-          break;
+          return num_test;
+          //break;
       }
-
-      return num_test;
     }
 };
 
 
 int main(int argc, char **argv, char **env) {
+  
   std::unique_ptr<SIMULATIONTB> tb(new SIMULATIONTB());
 
   tb->OpenTrace("imm_gen.vcd");
