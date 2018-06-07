7c375ed064bc582f756c2b59c5f11af51e52a6aa
Manuel Alejandro Hurtado <38893815+mhurtadolinc@users.noreply.github.com>
2018-06-07 15:27:28 -0400

    Update scr1.v

--- rtl/scr1.v
+++ rtl/scr1.v
@@ -1,4 +1,4 @@
-module scr1(clk_i, rst_i,address_i, en_write_i, en_read_i, data_i, en_except, data_out_o, mtvec_o);
+module scr1(clk_i, rst_i,address_i, en_write_i, en_read_i, data_i, en_except_i, data_out_o, mtvec_o);
 
     input clk_i;
     input rst_i; 
