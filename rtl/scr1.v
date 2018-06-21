fa470963b55ce11e8067c6707375fac20c6d877c
Manuel Alejandro Hurtado <38893815+mhurtadolinc@users.noreply.github.com>
2018-06-21 09:29:03 -0400

    Update scr1.v
    prueba por error de [ esperado en linea 102

--- rtl/scr1.v
+++ rtl/scr1.v
@@ -99,10 +99,10 @@ reg [31:0] register[31:0]; // estructura de 18 registros de 32 bits
     if (en_except && en_write_i)   begin
         // CASE PARA ESCRIBIR EN REGISTROS SI ESTA ACTIVA LA EXCEPCION
           case (address_i) 
-          32'b001101000010 : register[32'b00110][31:0] <= data_i; // 6 mcause				0x342
-          32'b001100000000 : register[32'b00111][31:0] <= data_i; // 7 mtatus				0x300
-          32'b001100000101 : register[32'b01000][31:0] <= data_i; // 8 mtvec				0x305
-          32'b001101000001 : register[32'b01001][31:0] <= data_i; // 9 mepc				0x341
+    //      32'b001101000010 : register[32'b00110][31:0] <= data_i; // 6 mcause				0x342
+    //      32'b001100000000 : register[32'b00111][31:0] <= data_i; // 7 mtatus				0x300
+    //      32'b001100000101 : register[32'b01000][31:0] <= data_i; // 8 mtvec				0x305
+    //      32'b001101000001 : register[32'b01001][31:0] <= data_i; // 9 mepc				0x341
           default;
         endcase 
     end
